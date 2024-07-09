337a285b300d93db4562248a8eafe213f55ae3fe
Ubuntu <ubuntu@ip-172-31-35-190.ap-south-1.compute.internal>
2024-07-09 02:13:44 +0000

    first stream commit

---
+++ src/main/java/com/example/SimpleStreamsApp.java
@@ -0,0 +1,28 @@
+package com.example;
+
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import java.util.Properties;
+
+public class SimpleStreamsApp {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "simple-streams-app");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-13-201-59-31.ap-south-1.compute.amazonaws.com:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("productevent");
+        source.to("adevent");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
