1f6231980cdd4924b8dd16995301612077a76511
Ubuntu <ubuntu@ip-172-31-35-190.ap-south-1.compute.internal>
2024-08-05 18:19:47 +0000

    added jenkins file

--- src/main/java/com/example/SimpleStreamsApp.java
+++ src/main/java/com/example/SimpleStreamsApp.java
@@ -4,7 +4,6 @@ package com.example;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -16,16 +15,16 @@ public class SimpleStreamsApp {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "simple-streams-app");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-13-201-59-31.ap-south-1.compute.amazonaws.com:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "ec2-13-201-94-176.ap-south-1.compute.amazonaws.com:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
 
         Map<String, String> hardcodedMap = new HashMap<>();
-        hardcodedMap.put("Communication", "[\"a\", \"b\"]");
-        hardcodedMap.put("healthcare", "[\"c\", \"d\"]");
-        hardcodedMap.put("clothing", "[\"e\"]");
+        hardcodedMap.put("Communication", "[\"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/1b011c536d905180.jpg?q=20\"]");
+        hardcodedMap.put("Wearables", "[\"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/04bb152bb2d3f429.jpg?q=20\"]");
+        hardcodedMap.put("Clothing", "[\"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/8b4e46e572ce96b2.jpg?q=20\"]");
 
         KStream<String, String> source = builder.stream("productevent");
 
